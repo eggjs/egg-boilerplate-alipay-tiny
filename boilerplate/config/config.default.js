0e24efb866df1c363a0d49b83dcb32eb6eb67792
popomore <sakura9515@gmail.com>
2018-08-15 16:32:22 +0800

    feat: first implement

--- boilerplate/config/config.default.js
+++ boilerplate/config/config.default.js
@@ -9,5 +9,23 @@ module.exports = appInfo => {
   // add your config here
   config.middleware = [];
 
+  config.security = {
+    csrf: {
+      ignoreJSON: false,
+    },
+  };
+
+  config.mysql = {
+    client: {
+      host: '127.0.0.1',
+      port: '3306',
+      user: 'root',
+      password: 'miniappmysql123',
+      database: '',
+    },
+    app: true,
+    agent: false,
+  };
+
   return config;
 };
